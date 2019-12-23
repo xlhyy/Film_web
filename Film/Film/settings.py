ed4caa27ce43f2d5d7587cd8b28401f9140c7713
ying.li <ying.li@kaiqigu.com>
2019-12-23 21:22:05 +0800

    fix

--- Film/Film/settings.py
+++ Film/Film/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nyx8y4fx)s-g5_3b)!_f=h$d_$mf%3yjdxaq&bgfktc)*3fz@9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
