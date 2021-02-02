b711c0ed0eb1027fabab3a49330c77ff787a9649
ying.li <xiaolouhuayaoyan@163.com>
2021-02-02 18:59:40 +0800

    fix

--- Film/Film/settings.py
+++ Film/Film/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'film',
         'USER': 'root',
-        'PASSWORD': 'kaiqigu1',
+        'PASSWORD': 'Root20200718',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
